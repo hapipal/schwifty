c7d618dc5f6c8d8395e73c721fa394bf8f9d8d75
WilliamSWoodruff <williamswoodruff@gmail.com>
2016-12-27 10:56:52 -0500

    WIP rearrange-for-migration

--- test/index.js
+++ test/index.js
@@ -305,34 +305,6 @@ describe('Schwifty', () => {
                 done();
             });
         });
-
-        it('throws when `migration` options are specified more than once.', (done) => {
-
-            const options = getOptions();
-            options.migration = {
-                dir: Path.normalize('./'),
-                mode: 'create'
-            };
-
-            getServer(options, (err, server) => {
-
-                expect(err).to.not.exist();
-
-                const plugin = (srv, opts, next) => {
-
-                    srv.register({ options, register: Schwifty }, next);
-                };
-
-                plugin.attributes = { name: 'my-plugin' };
-
-                expect(() => {
-
-                    server.register(plugin, () => done('Should not make it here.'));
-                }).to.throw('Schwifty\'s migration options can only be specified once.');
-
-                done();
-            });
-        });
     });
 
     describe('server.schwifty() decoration', () => {
