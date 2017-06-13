0a07d375dde60e922ee7c06281b961f5ccc8cfa4
Matt Boutet <matt@bigroomstudios.com>
2017-06-12 20:22:27 -0400

    Wrap up test coverage, make error prettier

--- test/index.js
+++ test/index.js
@@ -334,7 +334,65 @@ describe('Schwifty', () => {
             server.initialize((err) => {
 
                 expect(err).to.exist();
-                expect(err.message).to.startWith('Model');
+                expect(err.message).to.startWith('Model Collision attribute ');
+
+                done();
+            });
+        });
+    });
+
+    it('throws a pluralized error if there is a naming collision between multiple attributes and relations.', (done) => {
+
+        const Collision = class Collision extends Schwifty.Model {
+            static get tableName() {
+
+                return 'Collision';
+            }
+
+            static get joiSchema() {
+
+                return Joi.object({
+                    id: Joi.number().integer(),
+                    badName: Joi.string(),
+                    anotherBadName: Joi.string()
+                });
+            }
+
+            static get relationMappings() {
+
+                return {
+                    badName: {
+                        relation: Schwifty.Model.BelongsToOneRelation,
+                        modelClass: require('./models/person'),
+                        join: {
+                            from: 'Collision.badName',
+                            to: 'Person.firstName'
+                        }
+                    },
+                    anotherBadName: {
+                        relation: Schwifty.Model.BelongsToOneRelation,
+                        modelClass: require('./models/person'),
+                        join: {
+                            from: 'Collision.anotherBadName',
+                            to: 'Person.firstName'
+                        }
+                    }
+                };
+            }
+        };
+        getServer(getOptions({
+            models: [
+                TestModels.Person,
+                Collision
+            ]
+        }), (err, server) => {
+
+            expect(err).not.to.exist();
+
+            server.initialize((err) => {
+
+                expect(err).to.exist();
+                expect(err.message).to.startWith('Model Collision attributes');
 
                 done();
             });
